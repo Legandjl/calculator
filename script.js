32a46c625d4168a7e736b6f1cd9b63dc71675c8e
dorian <dorianlegan@gmail.com>
2021-07-05 01:56:22 +0100

    refactored further

--- script.js
+++ script.js
@@ -9,39 +9,40 @@ const operatorsList = ["+", "-", "*", "/", "="];
 let currentEval = {};
 let lastOp = "";
 
-numbers.forEach(function(number) {
+numbers.forEach(function (number) {
 
- 
     number.addEventListener("click", buttonclick);
 });
 
-operators.forEach(function(operator) {
+operators.forEach(function (operator) {
 
     operator.addEventListener("click", operatorClick)
-    
+
 });
 
-let add = function(num1, num2) {
+//calculator functionality
+
+let add = function (num1, num2) {
 
     return num1 + num2;
 
 }
 
-let subtract = function(num1, num2) {
+let subtract = function (num1, num2) {
 
     console.log("here");
 
     return num1 - num2;
 }
 
-let multiply = function(num1, num2) {
+let multiply = function (num1, num2) {
 
     return num1 * num2;
 }
 
-let divide = function(num1, num2) {
+let divide = function (num1, num2) {
 
-    return (num1/num2)
+    return (num1 / num2).toPrecision(4);
 }
 
 function operate(operator, num1, num2) {
@@ -54,85 +55,84 @@ function operate(operator, num1, num2) {
     }
     let calc = operators[operator];
 
-    return calc(parseFloat(num1), parseFloat(num2));
+    let result = calc(parseFloat(num1), parseFloat(num2));
+
+    if (isInt(result)) {
+
+        return result;
+    }
+
+    return parseFloat(result).toPrecision(4);
 }
 
+//gui
 
-function buttonclick(e) {        
+function buttonclick(e) {
 
-    currentInput += e.target.innerText; 
-    display.innerText += e.target.innerText;    
+    currentInput += e.target.innerText;
+    updateDisplayText(e.target.innerText);
 
     if (display.innerText.length > 15) {
 
         setDisplay(display.innerText.slice(1));
-    }    
+    }
 
 }
 
-function operatorClick(e) {  
-    
-    if(e.target.innerText == "=") {
+function operatorClick(e) {
+
+    if (e.target.innerText == "=" && currentInput!= "") {
 
         handleEvaluation(getLastOp());
         setLastOp("");
         return;
     }
 
-    if(checkEvalForNum1() === false && currentInput != "") {
+    if (checkEvalForNum1() === false && currentInput != "") {
         getCurrentEval().num1 = currentInput;
-        setLastOp(e.target.innerText);     
-        setDisplay(getCurrentEval().num1 + e.target.innerText)   
+        setLastOp(e.target.innerText);
+        setDisplay(getCurrentEval().num1 + e.target.innerText)
         currentInput = "";
         return;
     }
 
-    if(checkEvalForNum1() == true && currentInput == "") {
+    if (checkEvalForNum1() == true && currentInput == "" && e.target.innerText != "=") {       
 
         setLastOp(e.target.innerText);
-        setDisplay(getCurrentEval().num1 + e.target.innerText)   
-    }
-  
+        setDisplay(getCurrentEval().num1 + e.target.innerText)
+        return;
 
-    else if((checkEvalForNum1() === true && currentInput != "")) {   
+    } else if ((checkEvalForNum1() === true && currentInput != "")) {        
 
-        handleEvaluation(e.target.innerText);   
-        setLastOp(e.target.innerText); 
-        display.innerText += getLastOp();     
-    
+        handleEvaluation(e.target.innerText);
+        setLastOp(e.target.innerText);
+        updateDisplayText(getLastOp)();
+
     }
 
 }
+
+
+//helper functions
 
 function handleEvaluation(op) {
 
     getCurrentEval().num2 = currentInput;
-    getCurrentEval().operator = getLastOp();  
+    getCurrentEval().operator = getLastOp();
     let total = calculate(getCurrentEval());
     resetValues();
-    getCurrentEval().num1 = total;    
-
-    if(isInt(total)){
-        setDisplay(total );
-    }
-
-    else {
-
-        setDisplay(total.toPrecision(4));
-    }
-
+    getCurrentEval().num1 = total;
+    setDisplay(total);
 }
 
-function calculate(number) {  
+function calculate(number) {
 
     let num1 = parseFloat(number.num1);
     let num2 = parseFloat(number.num2);
     let operator = number.operator;
-    return  operate(operator, num1, num2); 
-  
+    return operate(operator, num1, num2);
+
 }
-
-
 
 function isInt(n) {
 
@@ -168,19 +168,26 @@ function getCurrentEval() {
 
 function checkEvalForOperator() {
 
-    let evaluation = getCurrentEval(); 
+    let evaluation = getCurrentEval();
 
     return ("operator" in evaluation);
 }
 
 function checkEvalForNum1() {
 
-    let evaluation = getCurrentEval();   
+    let evaluation = getCurrentEval();
 
     return ("num1" in evaluation);
 }
 
-function setDisplay(text){
+function setDisplay(text) {
 
     display.innerText = text;
 }
+
+function updateDisplayText(text) {
+
+    display.innerText += text;
+}
+
+
